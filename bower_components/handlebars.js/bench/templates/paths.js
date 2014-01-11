e9b413d8f8676f1feddcd432aa811a563674869f
Wiljan Slofstra <wiljanslofstra@gmail.com>
2014-01-11 16:43:44 +0100

    Initial commit

--- bower_components/handlebars.js/bench/templates/paths.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  context: { person: { name: "Larry", age: 45 } },
-  handlebars: "{{person.name}}{{person.age}}{{person.foo}}{{animal.age}}",
-  dust: "{person.name}{person.age}{person.foo}{animal.age}",
-  eco: "<%= @person.name %><%= @person.age %><%= @person.foo %><% if @animal: %><%= @animal.age %><% end %>",
-  mustache: "{{person.name}}{{person.age}}{{person.foo}}{{animal.age}}"
-};
--- bower_components/handlebars.js/src/parser-suffix.js
+++
@@ -1,2 +0,0 @@
-export default handlebars;
-/* jshint ignore:end */
