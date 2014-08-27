3d030bc49b8e2aca68588475f7d26a33b1801640
Wiljan Slofstra <wiljanslofstra@gmail.com>
2014-08-27 20:16:08 +0200

    Simplified.

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Gliese is a development framework that uses
 - [SASS](http://sass-lang.com/)
 - [Bower](https://github.com/bower/bower) package manager
 - [Bootstrap](http://getbootstrap.com/) JS/SASS (Optional)
-- [Gulp](http://gulpjs.com/) Build system (Optional)
 
 ## Prerequisites
 - [NPM](http://nodejs.org/)
@@ -21,9 +20,5 @@ gem install sass
 and to setup the project
 ```
 bower install
-gulp watch
-```
-or without gulp just run
-```
 sass --watch css/main.scss
 ```
--- elements/header.php
+++ elements/header.php
@@ -13,11 +13,11 @@
 		<link rel="apple-touch-icon-precomposed" href="icons/apple-touch-icon.png">
 		<link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon" />
 
-		<link rel="stylesheet" href="build/css/main.min.css">
+		<link rel="stylesheet" href="css/main.min.css">
 
 		<!--[if lte IE 8]>
-		 	<script src="build/js/bower_components/respond/dest/respond.min.js"></script>
-			<script src="build/css/ie/html5shiv.js"></script>
+		 	<script src="js/bower_components/respond/dest/respond.min.js"></script>
+			<script src="css/ie/html5shiv.js"></script>
 	  	<![endif]-->
 	</head>
 
