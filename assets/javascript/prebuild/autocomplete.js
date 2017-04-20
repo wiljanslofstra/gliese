877d2c00a97d3c6a90db8b13b3857acd9cac87ee
Wiljan Slofstra <wiljan@orangetalent.nl>
2017-04-20 20:46:31 +0200

    Add inline documentation to the autocomplete component

--- assets/javascript/prebuild/autocomplete.js
+++ assets/javascript/prebuild/autocomplete.js
@@ -5,14 +5,20 @@ import normalizeForSearch from 'normalize-for-search';
 const $autocomplete = $('.js-autocomplete');
 
 const autocomplete = {
+
+  /**
+   * Initialize the autocomplete on a single input field
+   * @param {Node} el DOM element of the input field
+   * @return {Void}
+   */
   initialize(el) {
-    const $el = $(el);
-    const data = $el.data();
+    this.$el = $(el);
+    this.data = this.$el.data();
 
-    this.$el = $el;
+    // Get the referenced template and compile it for later
+    this.template = template($(this.data.autoTemplate).html());
 
-    this.template = $(data.autoTemplate).html();
-
+    // Create the Awesomplete instance
     this.awesomplete = new Awesomplete(el, {
       list: [],
       filter: this.filter.bind(this),
@@ -20,11 +26,22 @@ const autocomplete = {
       replace: this.replace.bind(this),
     });
 
-    $el.on('keyup', () => {
-      const val = $el.val();
-      const api = data.autoApiUrl;
-      const fieldKey = data.autoApiKey;
+    // Create necessary events for handling API request etc.
+    this.createEvents();
+  },
 
+  /**
+   * Create events for example when the input changes
+   * @return {Void}
+   */
+  createEvents() {
+    // Listen for changes to the input
+    this.$el.on('keyup', () => {
+      const val = this.$el.val();
+      const api = this.data.autoApiUrl;
+      const fieldKey = this.data.autoApiKey;
+
+      // Fetch a new list from the API
       $.ajax({
         url: api,
         data: {
@@ -34,13 +51,19 @@ const autocomplete = {
       });
     });
 
-    $el.on('awesomplete-select', (e) => {
-      if (!data.autoCloseClick) {
+    // Handles the select event, we can use this to prevent closing
+    this.$el.on('awesomplete-select', (e) => {
+      if (!this.data.autoCloseClick) {
         e.preventDefault();
       }
     });
   },
 
+  /**
+   * Set the fetch list on the Awesomplete instance
+   * @param {String/Object} res Response from the API, either a object or a JSON string
+   * @return {Void}
+   */
   outputList(res) {
     let list = '';
 
@@ -54,11 +77,19 @@ const autocomplete = {
       list = res;
     }
 
+    // Set the new list
     this.awesomplete._list = list; // eslint-disable-line
 
+    // Re-evaluate so that the new list is properly propagated
     this.awesomplete.evaluate();
   },
 
+  /**
+   * Handles the filtering of data, this could also return true to disable filtering
+   * @param {Object} obj Object returned from the API
+   * @param {String} input User input from the input field
+   * @return {Boolean} If this item should be shown to the user
+   */
   filter(obj, input) {
     const title = normalizeForSearch(obj.value.title);
     const newInput = normalizeForSearch(input);
@@ -66,12 +97,20 @@ const autocomplete = {
     return title.indexOf(newInput) === 0;
   },
 
+  /**
+   * Render a single item in the Awesomplete dropdown
+   * @param {Object} obj Object of the current item returned from the API
+   * @return {Node} DOM node to render
+   */
   item(obj) {
-    const compile = template(this.template);
-
-    return $(compile(obj.value))[0];
+    return $(this.template(obj.value))[0];
   },
 
+  /**
+   * Handles the replacing of text in the input field when a item is selected
+   * @param {Object} obj Object of the current item returned from the API
+   * @return {Void}
+   */
   replace(obj) {
     if (typeof obj.value.redirect_url !== 'undefined') {
       window.location = obj.value.redirect_url;
