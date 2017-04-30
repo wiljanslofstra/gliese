68a74a9112ce64603fb091b545ad16b417d0e971
Wiljan Slofstra <wiljan@orangetalent.nl>
2017-04-30 19:36:10 +0200

    Fix service worker causing issues on non-cacheable requests

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v4';
+const PRECACHE = 'precache-v5';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -43,9 +43,6 @@ self.addEventListener('activate', (event) => {
     )).then(() => self.clients.claim()));
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener('fetch', (event) => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
@@ -56,9 +53,9 @@ self.addEventListener('fetch', (event) => {
         }
 
         return caches.open(RUNTIME).then(cache => (
-          fetch(event.request).then(response => (
-            cache.put(event.request, response.clone()).then(() => response)
-          ))
+          fetch(event.request).then((response) => {
+            return response;
+          })
         ));
       }));
   }
