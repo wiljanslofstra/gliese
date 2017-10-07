c309b4e9d3ad2094aa7853042d65a42ba13445bc
Wiljan Slofstra <wiljan@orangetalent.nl>
2017-10-07 20:31:03 +0200

    Only minify images when changed

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -2,6 +2,7 @@
 
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const changed = require('gulp-changed');
 const abs = require('../utils/abs');
 
 const src = abs(global.PATHS.images.src, global.PATHS.images.ext);
@@ -9,6 +10,7 @@ const dest = abs(global.PATHS.images.dest);
 
 function images() {
   return gulp.src(src)
+    .pipe(changed(dest))
     .pipe(imagemin([
       imagemin.gifsicle({ interlaced: true }),
       imagemin.jpegtran({ progressive: true }),
--- index.php
+++ index.php
@@ -2,56 +2,88 @@
 
 <style>
     /* DEMO STYLES */
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        background: #ccc url(/assets/dist/images/bg.jpg) no-repeat center center;
+        background-size: cover;
+    }
+
+    .demo-block {
+        background-color: #fff;
+        border-radius: 1rem;
+        margin: 2rem auto;
+    }
+
     .demo-block__code {
-        background: #6844d4;
-        border-radius: 5px;
+        background: #eee;
+        border-radius: 0 0 1rem 1rem;
+        margin: 0;
+        border-bottom: 3px solid #aaa;
     }
 
     .demo-block__heading {
+        font-size: 3rem;
         font-weight: 300;
         letter-spacing: .025em;
     }
 
-    body {
-        background-color: #7950f2;
-        color: #fff;
-    }
-
     .btn--primary {
         background-color: #fff;
-        color: #7950f2;
-        border: 2px solid transparent;
+        color: #888;
+        border: 2px solid #ddd;
         line-height: calc(2.5rem - 4px);
+        border-radius: 5rem;
     }
 
     .btn--primary:hover,
     .btn--primary:focus {
-        background-color: transparent;
-        border: 2px solid #fff;
-        color: #fff;
+        background-color: #ddd;
+        border: 2px solid #ddd;
+        color: #555;
+    }
+
+    .demo-block .btn {
+        margin-left: .33rem;
+        margin-right: .33rem;
+    }
+
+    @media (min-height: 480px) {
+        .demo-block {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+            margin: 0;
+        }
     }
 </style>
 
 <div class="container">
-    <main id="main" class="demo-block u-mt2 u-mb2 u-sm-mt3 u-sm-mb3 u-mx-auto u-text-center u-capped-lg" tabindex="-1">
-        <svg class="u-mb2" width="120" height="120" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><ellipse id="a" cx="60" cy="60" rx="60" ry="60"/><mask id="b" x="0" y="0" width="120" height="120" fill="#fff"><use xlink:href="#a"/></mask></defs><g fill="none" fill-rule="evenodd"><use stroke="#FFF" mask="url(#b)" stroke-width="14" xlink:href="#a"/><circle fill="#FFF" cx="41" cy="81" r="12"/><circle fill="#FFF" cx="78" cy="103" r="12"/><circle fill="#FFF" cx="45.5" cy="102.5" r="4.5"/><circle fill="#FFF" cx="20" cy="66" r="6"/><circle fill="#FFF" cx="55" cy="7" r="6"/><circle fill="#FFF" cx="111" cy="72" r="6"/><path d="M65.148 70.444c7.28 6.406 16.427 6.3 25.14 1.065 8.71-5.236 12.696-13.608 10.5-22.97-.78-3.256-2.842-7.504-5.438-11.825l-.303-.504c-1.6-2.663-4.02-3.266-6.685-1.665L78.282 40.6c-2.736 1.645-3.47 4.34-1.955 6.86 1.514 2.52 4.237 3.138 6.973 1.494l3.312-1.99c.288-.174.52-.116.692.172 1.125 1.872 1.976 3.615 2.323 5.17.852 3.702-1.076 7.506-4.82 9.756-4.248 2.553-8.167 2.065-11.237-.598-1.658-1.454-3.302-3.7-7.11-10.036-3.806-6.336-4.974-8.77-5.48-10.917-.953-4.032.384-7.677 4.56-10.186 3.168-1.904 6.15-2.324 9.58-.856 2.65 1.15 4.437.86 6.237-.22 2.664-1.6 3.656-4.843 1.97-7.65-.693-1.153-1.66-1.945-2.84-2.607-5.232-2.833-12.65-2.786-20.57 1.973-8.713 5.235-13.102 13.262-10.863 22.697.997 4.105 2.353 7.014 6.68 14.214 4.325 7.2 6.257 9.763 9.414 12.57z" fill="#FFF"/></g></svg>
+    <main id="main" class="demo-block u-text-center u-capped-md" tabindex="-1">
+        <div class="u-p2 u-md-p3">
+            <h1 class="demo-block__heading u-mb2 u-sm-mb3">
+                Gliese
+            </h1>
 
-        <h1 class="demo-block__heading u-mb2 u-sm-mb3">
-            Gliese
-        </h1>
+            <div>
+                <a class="btn btn--primary btn--icon btn--icon-after" href="dev/docs" role="button">
+                    Docs<?= getIcon('chevron-right'); ?>
+                </a>
 
-        <div class="u-mb2 u-sm-mb3">
-            <a class="btn btn--primary btn--icon btn--icon-after u-ml1 u-mr1" href="dev/docs" role="button">
-                Docs<?= getIcon('chevron-right'); ?>
-            </a>
+                <a class="btn btn--primary btn--icon btn--icon-after" href="dev/tools" role="button">
+                    Tools<?= getIcon('chevron-right'); ?>
+                </a>
 
-            <a class="btn btn--primary btn--icon btn--icon-after u-ml1 u-mr1" href="dev/tools" role="button">
-                Tools<?= getIcon('chevron-right'); ?>
-            </a>
-
-            <a class="btn btn--primary btn--icon btn--icon-after u-ml1 u-mr1" href="https://github.com/wiljanslofstra/Gliese" target="_blank" role="button">
-                Repository<?= getIcon('chevron-right'); ?>
-            </a>
+                <a class="btn btn--primary btn--icon btn--icon-after" href="https://github.com/wiljanslofstra/Gliese" target="_blank" role="button">
+                    Repository<?= getIcon('chevron-right'); ?>
+                </a>
+            </div>
         </div>
 
         <pre class="demo-block__code u-p2 u-text-left"><code>1. npm install / yarn install
